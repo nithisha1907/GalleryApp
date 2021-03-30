3c10393ba817967260ecf5b25b38784e0f5f6bdc
Nithisha <nithisha.1907@gmail.com>
2021-03-29 23:20:40 -0400

    Gallery App

--- GalleryApp/AppDelegate.swift
+++ GalleryApp/AppDelegate.swift
@@ -6,31 +6,69 @@
 //
 
 import UIKit
+import Photos
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let photos = PHPhotoLibrary.authorizationStatus()
+        if photos == .notDetermined {
+            PHPhotoLibrary.requestAuthorization({status in
+                if status == .authorized{
+                    self.gotoVC()
+                } else {
+                    let alert = UIAlertController(title: "Photos Access Denied", message: "App needs access to photos library.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.window?.rootViewController?.present(alert, animated: true, completion: nil)
+                }
+            })
+        } else if photos == .authorized {
+            gotoVC()
+        }
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func gotoVC() {
+        DispatchQueue.main.async(execute: { () -> Void in
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            if let window = self.window {
+                window.backgroundColor = UIColor.white
+                
+                let nav = UINavigationController()
+                let mainView = ViewController()
+                nav.viewControllers = [mainView]
+                window.rootViewController = nav
+                window.makeKeyAndVisible()
+            }
+        })
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationWillResignActive(_ application: UIApplication) {
+      
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+      
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
     }
 
 
 }
-
